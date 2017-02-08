fae7c25f9feb5c6e431da425031f38d6b096b700
Alan Buckley <alanbuckley@lbl.gov>
2017-02-08 15:59:25 -0800

    Merge 'master' changes to 'Controller' branch.

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from ScopeFoundryHW.ni_daq.hw.ni_dac_hw import NI_DAC_HW
 
 # Devices: NI tasks
 from ScopeFoundryHW.ni_daq.devices.NI_Daq import NI_AdcTask, NI_SyncTaskSet,  NI_CounterTask, NI_DacTask
+from .devices.ni_freq_counter import NI_FreqCounter
 
 # Measurements
 from ScopeFoundryHW.ni_daq.measure.adc_optimizer import AdcOptimizerMeasure
